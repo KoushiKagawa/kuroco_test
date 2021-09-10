829486a33e0f969794e8c62d7814722cebc51be9
Koushi Kagawa <kkagawa@diverta.co.jp>
2021-09-10 15:50:45 +0900

    edit

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'kuroco_test',
+    title: 'koushikagawa.com',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
