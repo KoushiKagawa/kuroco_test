dd05ec42b18acf0b0b131a582d8d6ed84ec91bdb
Koushi Kagawa <kkagawa@diverta.co.jp>
2021-12-16 14:19:49 +0900

    fix: css

---
+++ content/trial/test.md
@@ -0,0 +1,30 @@
+---
+title: Trial
+date: 2020.9.5
+---
+
+## Trial docs
+### H3
+#### H4
+
+---
+
+- aaa
+- bbb
+- ccc
+
+1. 111
+2. 222
+3. 333
+
+---
+
+### 画像はHTMLタグ
+<img src="/_nuxt/assets/images/heatghost.png" alt="heatghost" class="w-96 self-center mt-6 rounded-lg">
+
+### table
+
+| 項目   | 説明 |
+|:-------|:-------|
+| auto   | WebPをサポートしている場合、自動的にフォーマットを変換します。|
+| bgcolor | 背景色を設定します。|
---
+++ pages/trial/_slug.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <Header /> 
+        <main class="content">
+            <div class="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-3xl xl:px-0 m-4 break-words">
+                <h1>{{trial.title}}</h1>
+                <nuxt-content :document="trial" />
+            </div>
+        </main>
+        <Footer/>
+    </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+    components: {
+        Header,
+        Footer
+	},
+
+    async asyncData ({ $content, params }) {
+        const trial = await $content('trial', params.slug || 'index').fetch()
+        return { trial }
+    }
+}
+</script>
+
+<style scoped>
+table, td, th {
+    border-collapse: collapse;
+    border: 1px solid #333;
+}
+</style>
